c9a0060560164980576ee3874b2b98e05e7f8125
Hrridoy <hrridoyv2@gmail.com>
2020-09-30 15:48:36 +0600

    error

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBHcsHx4X8lXr0Paj3hMyAjHicANFa8Wo0",
+  authDomain: "ema-john-simple-29839.firebaseapp.com",
+  databaseURL: "https://ema-john-simple-29839.firebaseio.com",
+  projectId: "ema-john-simple-29839",
+  storageBucket: "ema-john-simple-29839.appspot.com",
+  messagingSenderId: "495271454101",
+  appId: "1:495271454101:web:dcde8164908c043523029c"
+};
+  export default firebaseConfig;
